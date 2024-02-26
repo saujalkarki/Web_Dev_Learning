e8682ca8165a1282cdfdf5d3d83b4117cb79a86b
Saujal Karki <saujalkarki04@gmail.com>
2024-02-26 20:37:42 +0545

    1st day of Js teaching/learning

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+// libraries framework
+
+// WebFront -- React-- Vue -- Angular
+// WebBack -- Nodejs runtime environment --express js framework
+// Mobile application Development -- React native -- cross platform app developpment
+// Desktop app development -- electron
+
+// There are 2 ways of writing Js:
+
+// Internal Javascript
+// External Javascript
+
+console.log("where are you from?");
