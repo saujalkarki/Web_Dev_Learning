fc3876f67c7ab2a3c2528db7204d2cd2334762b1
Saujal Karki <saujalkarki04@gmail.com>
2024-02-27 20:48:17 +0545

    Day 2 of learning js concepts

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// This code is written for learning js, coded by Saujal karki
+
 console.log("Hello World");
 
 // libraries framework
@@ -13,3 +15,106 @@ console.log("Hello World");
 // External Javascript
 
 console.log("where are you from?");
+
+// Day 2
+// 1.values and variables
+// 2.keywords in js
+// 3.comments
+// 4.Data types
+
+// Dynamic typing -- JS can indetify itself which kind of data is it
+// variables - value store garne thau lai
+// variable decleration - var let const
+// variable names - value store garne thau ko naam
+
+var name = "Saujal";
+let caste = "Karki";
+const gender = "male";
+// constant
+
+console.log(name);
+console.log(caste);
+console.log(gender);
+
+name = "Nischal";
+caste = "Nepal";
+// gender = "female";
+
+console.log(name);
+console.log(caste);
+console.log(gender);
+// let and var variables value's can be changed but const values cannot be
+
+// keywords
+// already used words by js
+// let var const for if function do while console log
+// shouldn't be used for our own purpose
+
+// Data types---------
+// 1 Number
+// 2 String
+// 3 Boolean
+// 4 Null
+// 5 Undefined
+// 6 Array
+// 7 Objects
+
+let age1 = 20; //number data types
+let age2 = "9815324015"; //String data types
+let age3 = false; // boolean data types --- true false
+let age4 = null; //null data types ---object
+let age5 = undefined; // undefined data types
+let age6 = [1, 2, 3, 4, 5]; // array-- object
+let age7 = ["nepal", "japan", "finland"]; // array-- object
+let age8 = ["nepal", 10, true]; // array-- object
+let age9 = {
+  name: "yudeen",
+  age: 18,
+  married: true,
+  marks: [1, 2, 3, 4, 5, 6],
+  classes: {
+    nepali: 2,
+    science: 6,
+    maths: "eight",
+    noClasses: false,
+  },
+}; // object
+
+console.log("saujal", "Nischal", 15, true, null);
+
+// The below code is for understanding data types
+
+console.log(age1, typeof age1);
+console.log(age2, typeof age2);
+console.log(age3, typeof age3);
+console.log(age4, typeof age4);
+console.log(age5, typeof age5);
+console.log(age6, typeof age6);
+console.log(age7, typeof age7);
+console.log(age8, typeof age8);
+console.log(age9, typeof age9);
+
+// Data types -- null Array Object
+
+// comments in js
+// They are the most important part of any programming language
+// There are 2 types of comments in js
+// 1 single line comment
+// 2 multi line comment
+
+// Rememberance
+
+// console.log(Saujalkarki);
+console.log("Saujal karki");
+
+// single line comment
+
+/* Multi line comment
+akfjdklsafjlds
+asdkfljlasfj
+asdlfkjlakfjlka
+asdfladksjf
+jasfjlda
+sdfj
+a
+*/
