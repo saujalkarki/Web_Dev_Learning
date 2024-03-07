1fa6daab0233d8141810262aca29a1e911c35554
Saujal Karki <saujalkarki04@gmail.com>
2024-03-07 20:36:38 +0545

    about strict mode and loops in js

---
+++ script1.js
@@ -0,0 +1,71 @@
+"use strict";
+// strict mode
+// helps to tackle with errors by throwing error message
+
+// let var const
+let country = "Nepal";
+
+console.log(country);
+
+// looping --
+// Entry control loop -- for , while -- ek choti matra print na hune case pani aaunna sakxa
+// Exit control loop -- do while -- ek choti chai print vayeraai xadxa
+
+// 1. for loop -- Entry control loop
+
+console.log("I love programming");
+console.log("I love programming");
+console.log("I love programming");
+console.log("I love programming");
+console.log("I love programming");
+console.log("I love programming");
+console.log("I love programming");
+console.log("I love programming");
+console.log("I love programming");
+console.log("I love programming");
+console.log("I love programming");
+
+// syntax
+// for(variable decleration; condition; increament/decreament){
+//     logic
+// }
+
+for (let i = 0; i < 4; i++) {
+  console.log("I love programming - for loop");
+}
+
+// i = 0, 0 < 4, 1 --- print 1
+// i = 1, 1 < 4, 2 --- print 2
+// i = 2, 2 < 4, 3 --- print 3
+// i = 3, 3 < 4, 4 --- print 4
+// i = 4, 4 < 4 -- false
+
+// 2. while loop -- Entry control loop
+// syntax
+// while (condition) {
+//   logic;
+//   increament / decreament;
+// }
+
+let j = 10;
+
+while (j < 20) {
+  console.log("I love programming -- while loop");
+  j++;
+}
+
+// 3. Do while loop -- exit control loop -- this loop will run atleast once a time
+// syntax
+// do {
+//   logic;
+// } while (condition);
+
+let z = 20;
+
+do {
+  console.log("I love programming -- do while loop");
+  z++;
+} while (z < 30);
+
+// Arrays
+// Arrays Methods
